b102af37bbcdeff4be9558c21ef79e6772bdb712
Youssef191107 <yousefelbanna25@gmail.com>
2024-09-02 15:10:57 +0300

    final

--- src/Routes/LoginComp.js
+++ src/Routes/LoginComp.js
@@ -9,7 +9,7 @@ import Main from "../Components/Main/Main";
 import Ready from "../Components/Ready/Ready";
 import Property from "../Components/Featured/Property";
 import Section from "../Components/Section/Section";
-import AboutUs from '../Components/AboutUs/About'
+import Slider from '../Components/Slider/Slider'
 import Subscribe from "../Components/Subscribe/Subscribe";
 import Footer from "../Components/Foooter/Foooter";
 const LoginComp = () => {
@@ -20,7 +20,7 @@ const LoginComp = () => {
       <Ready />
       <Property />
       <Section/>
-      <AboutUs/>
+      <Slider/>
       <Subscribe />
       <Footer />
       {/* <Login/> */}
